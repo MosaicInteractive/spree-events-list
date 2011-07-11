9354bd9979ea8053d09f0150835103a54d78f39e
Valentino Stoll <valentino@mosaicwebsite.com>
2011-07-11 11:48:26 -0400

    updated helper to pull only events after current date

--- app/helpers/events_list_helper.rb
+++ app/helpers/events_list_helper.rb
@@ -2,7 +2,7 @@ module EventsListHelper
 
   def recent_events_list(limit = Spree::Config[:event_list_recent])
     recent_event = Struct.new(:name, :url, :event_start, :event_end, :venue, :summary)
-    Event.publish.find(:all, :limit => limit).collect { |event| recent_event.new(event.title, "/events/show/#{event.id}", event.begin_event, event.end_event, event.venue, event.summary) }
+    Event.publish.find(:all, :limit => limit, :conditions => ["end_event > ?", DateTime.now], :order => 'begin_event').collect { |event| recent_event.new(event.title, "/events/show/#{event.id}", event.begin_event, event.end_event, event.venue, event.summary) }
   end
 
   def has_recent_events?
